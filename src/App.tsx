eed1bb20253afc2b9760cfedb9b103de0cd73f0a
Anna <annprogram-gh@mail.ru>
2022-02-22 12:10:06 +0300

    Check branch merge

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       Hello everyone!
       <div style={{color: 'red'}}><b>Create new branch!</b></div>
+      <div style={{color: 'blue'}}><b>Check branch merge opportunity!</b></div>
     </div>
   );
 }
